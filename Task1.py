61bcf796f95c621e4f0fab71a84d1f1613f87dee
firoz <firoz.ahmed2727@proton.me>
2025-11-08 10:02:50 +0530

    Added task1 and task2 solution

---
+++ Task1.py
@@ -0,0 +1,7 @@
+# Perform Basic Mathematical Operations
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+print("Addition: ",a+b)
+print("Substraction: ",a-b)
+print("Multiplication: ",a*b)
+print("Divison: ",a/b)
